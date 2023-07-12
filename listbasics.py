3c9c642015ed40ec992fc64325c4fa02171811e9
divyafrancis <divya@dolphitech.com>
2023-07-12 09:45:51 +0200

    Adding main and list basics python code

---
+++ listbasics.py
@@ -0,0 +1,26 @@
+empty_list=[]
+print(empty_list)
+
+list_str=['Apple','banana','grape','Papaya']
+print(list_str)
+
+print(type(list_str))
+
+list_int= [5,2,3,4,7]
+print(list_int)
+
+list_bool=[True,True,False]
+print(list_bool)
+
+list_mixed=['name',3,2.4,True]
+print(list_mixed[2])
+print(list_mixed[3]) 
+print("length og mixed list is: ",len(list_mixed))
+
+cars_list= ['audi','BMW','volkswagon','ambasidor']
+print("item at -2 position is: " , cars_list[-2])
+
+#replacing an item in lsit with new item
+cars_list[1]='lamborgini'
+print(cars_list)
+
---
+++ main.py
@@ -0,0 +1,11 @@
+
+print ("""this is first line 
+            this is second line
+            this is thrid line """)
+savings= 1000
+expense= 300
+bal= int(savings)-int(expense)
+print("savings is : ",savings)
+print("expense is :",expense)
+print("balance is :",bal)
+
